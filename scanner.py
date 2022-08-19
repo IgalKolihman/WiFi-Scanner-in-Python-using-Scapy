fdbdb97c591a285da02d3a63d7ce30f105370193
Igal Kolihman <igal.kolihman@gmail.com>
2022-08-19 17:47:14 +0300

    Code was formatted and added the option to choose which interface

--- scanner.py
+++ scanner.py
@@ -1,28 +1,49 @@
-from scapy.all import *
-from threading import Thread
-import pandas
-import time
+"""Scans a network interface for beacon packets (SSID)
+
+Usage:
+    scanner.py -h | --help
+    scanner.py -i <interface>
+
+Options:
+    -h --help   Show this help message
+    -i          Network interface to sniff packets from
+"""
 import os
+import time
+
+import docopt
+import pandas
+from threading import Thread
 
 # initialize the networks dataframe that will contain all access points nearby
-networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
+from scapy.sendrecv import sniff
+from scapy.layers.dot11 import Dot11Beacon, Dot11, Dot11Elt
+
+networks = pandas.DataFrame(
+    columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"]
+)
 # set the index BSSID (MAC address of the AP)
 networks.set_index("BSSID", inplace=True)
+
 
 def callback(packet):
     if packet.haslayer(Dot11Beacon):
         # extract the MAC address of the network
         bssid = packet[Dot11].addr2
+
         # get the name of it
         ssid = packet[Dot11Elt].info.decode()
         try:
             dbm_signal = packet.dBm_AntSignal
         except:
             dbm_signal = "N/A"
+
         # extract network stats
         stats = packet[Dot11Beacon].network_stats()
+
         # get the channel of the AP
         channel = stats.get("channel")
+
         # get the crypto
         crypto = stats.get("crypto")
         networks.loc[bssid] = (ssid, dbm_signal, channel, crypto)
@@ -45,15 +66,18 @@ def change_channel():
 
 
 if __name__ == "__main__":
-    # interface name, check using iwconfig
-    interface = "wlan0mon"
+    args = docopt.docopt(__doc__)
+    interface = args["<interface>"]
+
     # start the thread that prints all the networks
     printer = Thread(target=print_all)
     printer.daemon = True
     printer.start()
+
     # start the channel changer
     channel_changer = Thread(target=change_channel)
     channel_changer.daemon = True
     channel_changer.start()
+
     # start sniffing
     sniff(prn=callback, iface=interface)
